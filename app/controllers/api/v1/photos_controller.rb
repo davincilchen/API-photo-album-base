a489885c072d15166833381c58400d7edc783537
davincilchen <doki2099@gmail.com>
2018-04-12 20:52:42 +0800

    add index and show action for photo api

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -1,2 +1,33 @@
 class Api::V1::PhotosController < ApiController
+
+  def index
+    @photos = Photo.all
+    #render json: @photos
+    render json: {
+      data: @photos.map do |photo|{
+        title: photo.title,
+        date: photo.date,
+        description: photo.description
+      }
+      end
+    }
+  end
+
+  def show
+    #@photo = Photo.find(params[:id])
+    @photo = Photo.find_by(id: params[:id])
+    if !@photo
+      render json: {
+        message: "Can't find the photo!",
+        status: 400
+      }
+    else
+      render json: {
+        title: @photo.title,
+        date: @photo.date,
+        description: @photo.description
+      }
+    end
+  end
+
 end
